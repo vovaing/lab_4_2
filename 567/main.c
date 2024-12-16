31aacf96d69a90ba881d6a9dc9c9854b2fdb11a0
vovaing <palakangor@gmail.com>
2024-12-16 20:16:35 +0300

    Initial Commit

---
+++ 567/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  567
+//
+//  Created by Владимир Кюри on 16.12.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
